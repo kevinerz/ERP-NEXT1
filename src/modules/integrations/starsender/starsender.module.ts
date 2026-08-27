5fc5edc74fc476eb85815cbdce171eb6a53f3e39
kevinerz <kevinsamsung9@gmail.com>
2026-08-27 10:26:37 +0700

    fix(starsender): import AuthModule agar JwtAuthGuard bisa resolve TokenBlacklistService

--- src/modules/integrations/starsender/starsender.module.ts
+++ src/modules/integrations/starsender/starsender.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../../prisma/prisma.module';
+import { AuthModule } from '../../auth/auth.module';
 import { StarsenderClient } from './starsender.client';
 import { StarsenderService } from './starsender.service';
 import { StarsenderController } from './starsender.controller';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   providers: [StarsenderClient, StarsenderService],
   controllers: [StarsenderController],
   exports: [StarsenderService],
